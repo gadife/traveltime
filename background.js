0b823f7ffddf6926bfb68ed262dff49005fa11b3
Gadi Feldman <>
2016-11-28 07:20:40 +0200

    moveover

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	// There is a problem if the first url is the eventpage_6 one.
+	var oauth = ChromeExOAuth.initBackgroundPage({
+	  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+	  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+	  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+	  'consumer_key': 'anonymous',
+	  'consumer_secret': 'anonymous',
+	  'scope': 'https://www.googleapis.com/auth/calendar',
+	  'app_name': 'Travel Time'
+	});
 
 	if(changeInfo.status == 'complete' && tab.url.endsWith('eventpage_6')){
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	    	var activeTab = tabs[0];
 	    	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+	    	console.log("test");
 	    });
 	}
+	chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+  	// Use the token.
+	});
 })
--- content.js
+++ content.js
@@ -33,5 +33,21 @@ function add_travel_time_data(){
 								'</div>'
 
 	// parentNode.insertBefore(location_row, parentGuest.nextSibling);
-	parent_node.insertBefore(travel_row, location_row.nextSibling)
+	parent_node.insertBefore(travel_row, location_row.nextSibling);
+
+	// search for Save button element
+	var save = document.getElementsByClassName("goog-inline-block goog-imageless-button");
+	for (i=0;i<save.length;i++) {
+		console.log(save[i].textContent);
+		if (save[i].textContent.includes("Save")) {
+			console.log("got it");
+			var save_element = save[i];
+			break;
+		}
+	}
+	// Get event date and start time
+	save_element.addEventListener('mouseover', function(){
+		console.log(document.getElementsByClassName("text dr-time")[0].value);
+		console.log(document.getElementsByClassName("text dr-date")[0].value);
+	});
 }
